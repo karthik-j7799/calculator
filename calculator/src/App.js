b6a0b2801d4fd3ac878e65c24f4275a92d3a56e6
Karthik <kart33539@gmail.com>
2023-10-11 21:40:01 +0530

    Update App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [values,setValues] = useState("");
 
   const handleClick = (e) => {
-    setValues(values.concat(e.target.name));
+    setValues(values.concat(e.target.value));
   }
 
   const clearInput = () => {
@@ -42,21 +42,21 @@ function App() {
 
       <button onClick={clearInput} className="clear">C</button>
       <button onClick={backSpace} className="backspace">🔙</button>
-      <button name="/" onClick={handleClick}>/</button>
-      <button name="7" onClick={handleClick}>7</button>
-      <button name="8" onClick={handleClick}>8</button>
-      <button name="9" onClick={handleClick}>9</button>
-      <button name="*" onClick={handleClick}>*</button>
-      <button name="4" onClick={handleClick}>4</button>
-      <button name="5" onClick={handleClick}>5</button>
-      <button name="6" onClick={handleClick}>6</button>
-      <button name="-" onClick={handleClick}>-</button>
-      <button name="1" onClick={handleClick}>1</button>
-      <button name="2" onClick={handleClick}>2</button>
-      <button name="3" onClick={handleClick}>3</button>
-      <button name="+" onClick={handleClick}>+</button>
-      <button name="0" onClick={handleClick}>0</button>
-      <button name="." onClick={handleClick}>.</button>
+      <button value="/" onClick={handleClick}>/</button>
+      <button value="7" onClick={handleClick}>7</button>
+      <button value="8" onClick={handleClick}>8</button>
+      <button value="9" onClick={handleClick}>9</button>
+      <button value="*" onClick={handleClick}>*</button>
+      <button value="4" onClick={handleClick}>4</button>
+      <button value="5" onClick={handleClick}>5</button>
+      <button value="6" onClick={handleClick}>6</button>
+      <button value="-" onClick={handleClick}>-</button>
+      <button value="1" onClick={handleClick}>1</button>
+      <button value="2" onClick={handleClick}>2</button>
+      <button value="3" onClick={handleClick}>3</button>
+      <button value="+" onClick={handleClick}>+</button>
+      <button value="0" onClick={handleClick}>0</button>
+      <button value="." onClick={handleClick}>.</button>
       <button onClick={evaluateResult} className='result' >=</button>
 
       </div>
